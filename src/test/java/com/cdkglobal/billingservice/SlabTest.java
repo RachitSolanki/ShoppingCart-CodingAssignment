120e33605da7883b98516e21acef8ec88d171159
Rachit Solanki <rachit.solanki@outlook.com>
2020-06-23 00:41:07 +0530

    move to class scope

--- src/test/java/com/cdkglobal/billingservice/SlabTest.java
+++ src/test/java/com/cdkglobal/billingservice/SlabTest.java
@@ -5,16 +5,18 @@ import org.junit.Test;
 
 public class SlabTest {
 
+private float AMOUNT = 15000;
+
 @Test
 public void slabDiscountTest_1() {
         Slab slab = new Slab(5000,10000,10);
-        Assert.assertEquals(500,slab.calculateDiscount(15000),0);
+        Assert.assertEquals(500,slab.calculateDiscount(AMOUNT),0);
 }
 
 @Test
 public void slabDiscountTest_2() {
         Slab slab = new Slab(10000,Integer.MAX_VALUE,20);
-        Assert.assertEquals(1000,slab.calculateDiscount(15000),0);
+        Assert.assertEquals(1000,slab.calculateDiscount(AMOUNT),0);
 }
 
 }
